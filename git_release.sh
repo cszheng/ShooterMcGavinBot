4d20fad446be34bd11cfc2cc2cbbdd43d17ac864
Chang Shuo Zheng <chang.s.zheng@gmail.com>
2017-08-24 13:00:14 -0400

    git release as shell script attempt 3

--- git_release.sh
+++ git_release.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #create a release tag for the current build
-RELEASE_TAG=$RELEASE_VERSION.$TRAVIS_BUILD_NUMBER
-REMOTE_URL=https://$GIT_OAUTH_USER:$GIT_OAUTH_TOKEN@$GIT_REMOTE_URL     
+RELEASE_TAG=$1
+GIT_USER=$2
+GIT_TOKEN=$3
+GIT_URL=$4
+GIT_BRANCH=$5
+REMOTE_URL=https://$GIT_USER:$GIT_TOKEN@$GIT_URL     
 git remote rm origin
 git remote add origin $REMOTE_URL
 git tag $RELEASE_TAG -am "Generated tag from TravisCI build $RELEASE_TAG"
-git push origin $TRAVIS_BRANCH --tags
+git push origin $GIT_BRANCH --tags
