7f5bc49ec78a7fb1a280a16f9625b380a4e163eb
Chang Zheng <chang.s.zheng@gmail.com>
2017-09-11 16:35:46 -0400

    travisci release login info from encrypted files attempt 4

--- travis_scripts/after_success/after_success.sh
+++ travis_scripts/after_success/after_success.sh
@@ -3,5 +3,5 @@
 RELEASE_TAG=v$RELEASE_MAJOR.$RELEASE_MINOR.$RELEASE_BUGFIXES.$TRAVIS_BUILD_NUMBER
 
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$RELEASE" == "true" ]; then 
-    ./travis_script/release_git.sh $RELEASE_TAG $GIT_USER $GIT_TOKEN $GIT_URL $TRAVIS_BRANCH; 
+    ./travis_script/after_success/release_git.sh $RELEASE_TAG $GIT_USER $GIT_TOKEN $GIT_URL $TRAVIS_BRANCH; 
 fi
