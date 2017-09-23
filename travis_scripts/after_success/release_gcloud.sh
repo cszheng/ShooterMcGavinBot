9b20c6f8fac643fe346785fc830d80d8fe0391e9
Chang Zheng <chang.s.zheng@gmail.com>
2017-09-23 16:34:23 -0400

    gcloud auto deploy attempt 2

--- travis_scripts/after_success/release_gcloud.sh
+++ travis_scripts/after_success/release_gcloud.sh
@@ -3,8 +3,14 @@ gcloudssh () {
     gcloud compute ssh $GCLOUD_INSTANCE_USER@$GCLOUD_INSTANCE --ssh-key-file=gcloud-instance-key --command="$1"    
 }
 
-echo "if [ ! -d [HOME]/google-cloud-sdk ]; then"
-if [ ! -d ${HOME}/google-cloud-sdk ]; then
+echo "if [ ! -d \"[HOME]/google-cloud-sdk/bin\" ]; then"
+if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then
+
+    echo "rm -rf [HOME]/google-cloud-sdk"
+    rm -rf $HOME/google-cloud-sdk
+
+    echo "export CLOUDSDK_CORE_DISABLE_PROMPTS=1"
+    export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 
     echo "https://sdk.cloud.google.com | bash"
     curl https://sdk.cloud.google.com | bash
