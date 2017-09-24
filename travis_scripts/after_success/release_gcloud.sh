b14b1c9f1e091fe21afdc90c31f33ed6fa61d42a
Chang Zheng <chang.s.zheng@gmail.com>
2017-09-23 22:25:35 -0400

    gcloud release reattempt 4

--- travis_scripts/after_success/release_gcloud.sh
+++ travis_scripts/after_success/release_gcloud.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "curl http://[GCLOUD_URL]/?token=[GCLOUD_TOKEN]&hook=[GCLOUD_HOOK]"
-curl http://$GCLOUD_URL/?token=$GCLOUD_TOKEN&hook=$GCLOUD_HOOK
+curl "http://$GCLOUD_URL/?token=$GCLOUD_TOKEN&hook=$GCLOUD_HOOK"
